a9cf74a316a7fcc4eedd01fc2d37be35c838b58e
daniel <danielsa135@gmail.com>
2019-07-18 12:30:27 -0300

    tyuty

--- HTML/js/main.js
+++ HTML/js/main.js
@@ -24,3 +24,29 @@ $(function(){
 
 
 });
+
+$(function(){
+
+    //Ajax para Login
+    $("#FormCadastro").submit(function(){    
+        
+        var dados = $("#form-login").serialize();
+        $.ajax({
+            url: 'cadastro.php',
+            type: 'POST',
+            data: dados,
+            success: function(data){
+                data = JSON.parse(data);
+                $(".resposta-login").html(data.mensagem);
+                if(data.status){
+                //location.href = '';
+                } else {
+
+                }
+            }
+        });
+        return false;
+    });
+
+
+});
